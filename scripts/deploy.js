fa3f40c7bc371c5c95cf4d1f13d4474505dc6464
Vansh Shah <vanshshah842@gmail.com>
2024-02-29 09:27:16 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Create = await hre.ethers.getContractFactory("Create");
+  const create = await Create.deploy();
+
+  await create.deployed();
+
+  console.log("Voting: ", create.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
